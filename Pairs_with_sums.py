1cc07830271a71774d4bb761d7113f5fec4d3443
Olubusolami-R <78950241+Olubusolami-R@users.noreply.github.com>
2021-06-25 15:00:44 +0100

    pairs with sums commit

---
+++ Pairs_with_sums.py
@@ -0,0 +1,24 @@
+def pairs_with_sum(self, sum_val):  
+    cur1=self.head
+    cur2=self.head
+    strlist=[]
+    #checkDict=dict()
+    while cur1:
+      cur2=cur1
+      while cur2:
+
+        if (cur1.data+cur2.data)==sum_val:
+          a=str(cur1.data)
+
+          b=str(cur2.data)
+
+          c="("+a+","+b+")"
+
+          strlist.append(c)
+          
+        cur2=cur2.next
+
+      
+      cur1=cur1.next
+
+    return strlist
